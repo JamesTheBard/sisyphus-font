972d75ac988024006d0d231245fb83f24dab2581
jweatherly <jamesthebard@gmail.com>
2023-10-20 10:23:18 -0400

    Documentation and general cleanup.

--- fontscrape/fonts.py
+++ fontscrape/fonts.py
@@ -1,28 +1,46 @@
-from pathlib import Path
 from dataclasses import dataclass
-from typing import Union
-from loguru import logger
-from fuzzywuzzy import fuzz
-from typing import List, Optional
+from pathlib import Path
+from typing import List, Optional, Union
+
 from fontTools import ttLib
+from fuzzywuzzy import fuzz
+from loguru import logger
 
 
 @dataclass
 class Font:
+    """A Font object containing all the information for a processed font.
+
+    Attributes:
+        family (str): The font family.
+        subfamily (list[str]): The font subfamily.
+        full_name (str): The full name of the font.
+        font_path (Path): The filesystem location of the font.
+    """
     family: Optional[str] = None
     subfamily: Optional[list] = None
     full_name: Optional[str] = None
     font_path: Optional[Path] = None
-    
+
     def __eq__(self, other):
         return self.full_path == other.full_path
-    
+
     def __hash__(self):
         return hash(self.full_name)
 
 
 @dataclass
 class FontResult:
+    """The result of a font comparison.
+
+    FontResults can be compared against each other, where the `family_match_score` is compared first, then the `subfamily_match_score` afterwards.
+
+    Attributes:
+        font (Font): The Font that matched.
+        family_match_score (int): The 'confidence' of the family match. Range is from 0 to 100.
+        subfamily_match_score (int): The 'confidence' of the subfamily match. Range is from 0 to 100.
+        downgrade (bool): Whether the font was downgraded during the matching process.
+    """
     font: Font
     family_match_score: int
     subfamily_match_score: int
@@ -59,14 +77,27 @@ class FontResult:
 
 
 class FontLibrary:
+    """Create a font library from a given directory of fonts.
+
+    Attributes:
+        library (list[Font]): A list of Fonts containing font information from the fonts in the library path.
+        library_path (Union[Path, str]): The path of the directory containing the fonts to parse.
+    """
 
     library: List[Font]
+    library_path: Union[Path, str]
 
     def __init__(self, library_path: Union[Path, str]):
+        """Create the font library.
+
+        Args:
+            library_path (Union[Path, str]): The path of the directory containing the fonts to parse.
+        """
         self.library_path = Path(library_path)
         self.library = self.parse_library()
 
     def parse_library(self) -> List[Font]:
+        """Parse all of the fonts in the library."""
         results = list()
         for f in self.library_path.glob("*"):
             if f.suffix.lower() not in [".ttf", ".otf"]:
@@ -81,73 +112,141 @@ class FontLibrary:
             results.append(font)
         return results
 
-    def find_font(self, family: str, subfamily: Union[list[str], str], ignore_regular: bool = False, use_full_name: bool = False, downgrade: bool = False, threshold: int = 80) -> FontResult:
+    def find_font_by_families(self, family: str,
+                              subfamily: Union[list[str], str],
+                              ignore_regular: bool = False,
+                              downgrade: bool = False,
+                              threshold: int = 90) -> Optional[FontResult]:
+        """Find a font using a specific family and subfamily comparison.
+
+        Args:
+            family (str): The font family.
+            subfamily (Union[list[str]], str]): The font subfamily.
+            ignore_regular (bool, optional): Ignore the 'regular' subfamily when doing comparisons. Defaults to False.
+            downgrade (bool, optional): Allow the font to downgrade to the base font sans subfamily. Defaults to False.
+            threshold (int, optional): The threshold to match against. Defaults to 90.
+
+        Returns:
+            Optional[FontResult]: The FontResult match if it meets/exceeds the threshold, otherwise None.
+        """
         orig_subfamily = subfamily
         if isinstance(subfamily, str):
             subfamily = [subfamily.split(" ")]
         sfam = [i.lower() for i in subfamily]
         results = list()
 
-        if not use_full_name:
-            if ignore_regular:
-                subfamily = [i for i in subfamily if i not in ["bold", "italic"]]
+        if ignore_regular:
+            subfamily = [i for i in subfamily if i not in ["bold", "italic"]]
             subfamily = ' '.join(subfamily)
             for font in self.library:
                 sfam = subfamily
                 if ignore_regular:
-                    sfam = [i for i in font.subfamily if i not in ["bold", "italic"]]
+                    sfam = [i for i in font.subfamily if i not in [
+                        "bold", "italic"]]
                 sfam = ' '.join(sfam)
                 family_score = fuzz.ratio(family, font.family)
                 subfamily_score = fuzz.ratio(subfamily, sfam)
 
                 if family_score >= threshold:
-                    results.append(FontResult(font, family_score, subfamily_score))
+                    results.append(FontResult(
+                        font, family_score, subfamily_score))
             if not results and downgrade:
                 sfam = ' '.join(sfam)
                 family_score = fuzz.ratio(family, font.family)
 
                 if family_score >= threshold:
-                    results.append(FontResult(font, family_score, subfamily_score, True))
+                    results.append(FontResult(
+                        font, family_score, subfamily_score, True))
 
-        else:
+            try:
+                results = sorted(results)[-1]
+            except IndexError:
+                results = None
+
+            if not results:
+                subfamily = ', '.join(subfamily) if subfamily else "None"
+                logger.warning(
+                    f"Could not find font: {family}, subfamilies: {orig_subfamily}")
+            else:
+                font = results.font
+                dg_flag = "↓" if results.downgrade else " "
+                subfamily = ', '.join(font.subfamily) if subfamily else "None"
+                logger.debug(
+                    f"Found font: [{dg_flag}{results.family_match_score:>3}%] {font.family}, subfamilies: {subfamily}")
+
+            return results
+
+    def find_font_by_full_name(self, family: str,
+                               subfamily: Union[list[str], str],
+                               downgrade: bool = False,
+                               threshold: int = 90) -> Optional[FontResult]:
+        """Find a font by comparing the family/subfamily to the font's full name.
+
+        Args:
+            family (str): The font family.
+            subfamily (Union[list[str], str]): The font subfamily.
+            downgrade (bool, optional): Allow the font to downgrade to the base font sans subfamily. Defaults to False.
+            threshold (int, optional): The threshold to match against. Defaults to 90.
+
+        Returns:
+            Optional[FontResult]: The FontResult match if it meets/exceeds the threshold, otherwise None.
+        """
+        orig_subfamily = subfamily
+        if isinstance(subfamily, str):
+            subfamily = [subfamily.split(" ")]
+        results = list()
+        for font in self.library:
+            sfmt = ' '.join(i.capitalize() for i in subfamily)
+            full_name = f"{family} {sfmt}"
+            family_score = fuzz.ratio(
+                full_name.lower(), font.full_name.lower()
+            )
+            if family_score >= threshold:
+                results.append(FontResult(font, family_score, 0))
+        if not results:
             for font in self.library:
+                ffull_name = ' '.join([i.replace("semi", "") for i in [
+                                      j for j in font.full_name.lower().split(" ")]])
                 sfmt = ' '.join(i.capitalize() for i in subfamily)
                 full_name = f"{family} {sfmt}"
-                family_score = fuzz.ratio(full_name.lower(), font.full_name.lower())
+                family_score = fuzz.ratio(full_name.lower(), ffull_name)
                 if family_score >= threshold:
                     results.append(FontResult(font, family_score, 0))
-            if not results:
-                for font in self.library:
-                    ffull_name = ' '.join([i.replace("semi", "") for i in [j for j in font.full_name.lower().split(" ")]])
-                    sfmt = ' '.join(i.capitalize() for i in subfamily)
-                    full_name = f"{family} {sfmt}"
-                    family_score = fuzz.ratio(full_name.lower(), ffull_name)
-                    if family_score >= threshold:
-                        results.append(FontResult(font, family_score, 0))
-            if not results and downgrade:
-                for font in self.library:
-                    full_name = f"{family}"
-                    family_score = fuzz.ratio(full_name.lower(), font.full_name.lower())
-                    if family_score >= threshold:
-                        results.append(FontResult(font, family_score, 0, True))
+        if not results and downgrade:
+            for font in self.library:
+                full_name = f"{family}"
+                family_score = fuzz.ratio(
+                    full_name.lower(), font.full_name.lower())
+                if family_score >= threshold:
+                    results.append(FontResult(font, family_score, 0, True))
 
         try:
             results = sorted(results)[-1]
         except IndexError:
             results = None
-        
+
         if not results:
             subfamily = ', '.join(subfamily) if subfamily else "None"
-            logger.warning(f"Could not find font: {family}, subfamilies: {orig_subfamily}")  
+            logger.warning(
+                f"Could not find font: {family}, subfamilies: {orig_subfamily}")
         else:
             font = results.font
             dg_flag = "↓" if results.downgrade else " "
             subfamily = ', '.join(font.subfamily) if subfamily else "None"
-            logger.debug(f"Found font: [{dg_flag}{results.family_match_score:>3}%] {font.family}, subfamilies: {subfamily}")          
+            logger.debug(
+                f"Found font: [{dg_flag}{results.family_match_score:>3}%] {font.family}, subfamilies: {subfamily}")
         return results
 
-    def find_font_by_family(self, family: str, threshold: int = 80) -> List[FontResult]:
-        f_attrs = ["font", "preferred"]
+    def find_font_by_family(self, family: str, threshold: int = 90) -> List[FontResult]:
+        """Find a font by only comparing against the font family.
+
+        Args:
+            family (str): The font family.
+            threshold (int, optional): The threshold to match against. Defaults to 90.
+
+        Returns:
+            List[FontResult]: A list of FontResult matches that meet/exceed the threshold sorted by confidence (decending).
+        """
         results = list()
         for font in self.library:
             family_score = fuzz.ratio(family, font.family)
--- test.py
+++ test.py
@@ -14,9 +14,15 @@ for sub in subs:
     a = SubParse(sub)
     for style in a.styles:
         fonts.append(
-            fl.find_font(style.family, style.subfamily,
-                         use_full_name=True, downgrade=True, threshold=90)
+            # fl.find_font(style.family, style.subfamily,
+            #              use_full_name=True, downgrade=True, threshold=90)
+            fl.find_font_by_full_name(
+                family=style.family,
+                subfamily=style.subfamily,
+                downgrade=True
+            )
         )
+        
 
 fonts = set([i.font for i in fonts if i])
 [logger.info(f"Found font: {i.font_path}") for i in fonts]
